0428884b88ff0edd96442540340cf2d716f21b89
Stephen Wen <a5875246a@gmail.com>
2022-12-21 17:57:58 +0800

    Update get_streams_from_channel.py

--- get_streams_from_channel.py
+++ get_streams_from_channel.py
@@ -6,19 +6,24 @@ class get_streams_from_channel(object):
         """
         self.channel_url = channel_url
         # 下面排除尚未直播的影片url
-        test = get_channel(channel_url=self.channel_url,
-                           content_type='streams',
-                           limit=limit)
-        # 由於get_channel本身沒有跳過「尚未直播」的直播的功能，因此手動修改內部程式碼
-        # get_channel經過修改，達到可以跳過「即將直播」的直播
-        # change the code inside of get_channel, so that I can skip "coming soon" stream
-        test_list = []
-        for i in test:
-            test_list.append(i['videoId'])
+        test_limit = 5 # test_limit先抓5部近期直播，看下方test會回傳多少直播
+        test_list = [] # test_list儲存回傳直播數量，回傳如果只有3，表示有2部尚未開始的直播
+        while len(test_list) == 0: 
+            test = get_channel(channel_url=self.channel_url,
+                            content_type='streams',
+                            limit=test_limit)
+            # 由於get_channel本身沒有跳過「尚未直播」的直播的功能，因此手動修改內部程式碼
+            # get_channel經過我自己的修改，達到可以跳過「即將直播」的直播
+            # change the code inside of get_channel, so that I can skip "coming soon" stream
+            for i in test:
+                test_list.append(i['videoId'])
+            test_limit += 1 # 如果尚未開始的直播超過5部，就再+1探索真實數量
+        # 下面的(test_limit - len(test_list))就是尚未開始的直播數量，要加到limit上
+        real_limit = limit + (test_limit - len(test_list) - 1)
         # 排除完成
         self.videos = get_channel(channel_url=self.channel_url,
                      content_type='streams',
-                     limit=limit + (limit - len(test_list)))
+                     limit=real_limit)
         self.url_list = []
     
     def main(self):
