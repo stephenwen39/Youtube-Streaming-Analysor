51e20300b7babd01e33f9caa13a79abe070a4e14
Stephen Wen <a5875246a@gmail.com>
2023-03-24 22:29:32 +0800

    Update get_streams_from_channel.py

--- python_files/currently_using/get_streams_from_channel.py
+++ python_files/currently_using/get_streams_from_channel.py
@@ -1,13 +1,13 @@
 class get_streams_from_channel(object):
-    def __init__(self, channel_url, limit=20):
+    def __init__(self, channel_id, limit=20):
         """
-        channel_url: 頻道id連結，目前沒有簡單方法可以獲取channel id，請使用網頁開發者模式或此網站獲取https://commentpicker.com/youtube-channel-id.php
+        channel_id: 頻道id連結，目前沒有簡單方法可以獲取channel id，請使用網頁開發者模式或此網站獲取https://commentpicker.com/youtube-channel-id.php
         limit: 想要抓的直播url數量，預設20，limit=0的話會回傳empty list
         """
         self.url_list = []
-        self.channel_url = channel_url
+        self.channel_id = channel_id
         session = YouTubeChatDownloader()
-        generater = session.get_user_videos (channel_id = self.channel_url, 
+        generater = session.get_user_videos (channel_id = self.channel_id, 
                                              video_type = 'live', 
                                              params = None )
         self.limit = limit
@@ -23,6 +23,6 @@ class get_streams_from_channel(object):
             self.url_list.append('https://www.youtube.com/watch?v=' + i)
         return self.url_list
 '''
-get_streams_from_channel(channel_url='UC4J0GZLM55qrFh2L-ZAb2LA',
+get_streams_from_channel(channel_id='UC4J0GZLM55qrFh2L-ZAb2LA',
                          limit=5).main()
 '''
